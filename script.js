871b811a78e7627d937d6111059b1b2b0016be1a
Anthony-Oliveira <95887439+Anthony-Oliveira@users.noreply.github.com>
2022-04-05 15:09:30 -0300

    End of project Slick

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ $(document).ready(() => {
   const slickOptions = {
     autoplay: true,
     dots: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-    prevArrow:
-      '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
-    nextArrow:
-      '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
   };
 
   $(".slider").slick(slickOptions);
